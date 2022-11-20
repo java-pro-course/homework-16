a28d408dc46fe16d936ea37a24dc6a8643aa1359
chewedmole <chewedmole.git@gmail.com>
2022-11-21 00:30:09 +0700

    Pls, help me with task 3!

---
+++ kazakov/hw15/src/main/java/com/kazakov/hw15/object/User.java
@@ -0,0 +1,25 @@
+package com.kazakov.hw15.object;
+
+public class User {
+    private String name;
+    private String surname;
+    private int age;
+
+    public User(String name, String surname, int age){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
